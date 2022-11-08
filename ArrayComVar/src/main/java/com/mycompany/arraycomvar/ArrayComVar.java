4eb738a2c87fff17bf01aacce6e86d1ccdc3aa20
Marcos <marcos.henrique.a7x@@gmail.com>
2022-11-07 23:41:07 -0300

    iniciado arrayList com valores de variaveis

---
+++ ArrayComVar/src/main/java/com/mycompany/arraycomvar/ArrayComVar.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+package com.mycompany.arraycomvar;
+
+/**
+ *
+ * @author marck
+ */
+public class ArrayComVar {
+
+    // ainda está em desenvolvimento 
+    public static void main(String[] args) {
+        String a = "Teste1";
+        String b = "Teste2";
+        String c = "Teste3";
+
+        String arr[];
+    }
+}
